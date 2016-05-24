4e57ad0c46b4ed444319127cad16b3e7ea82364c
Leo Ryberg <leo@sentientdesign.se>
2016-05-24 10:41:24 +0200

    new repo

---
+++ javascript/app.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+	jQuery('img.svg').each(function(){
+		var $img = jQuery(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		jQuery.get(imgURL, function(data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = jQuery(data).find('svg');
+
+			// Add replaced image's ID to the new SVG
+			if(typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+			}
+			// Add replaced image's classes to the new SVG
+			if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+			}
+
+			// Remove any invalid XML tags as per http://validator.w3.org
+			$svg = $svg.removeAttr('xmlns:a');
+
+			// Replace image with new SVG
+			$img.replaceWith($svg);
+
+		}, 'xml');
+	});
+
+	resizeLogo();
+	bagFoldedOut = false;
+
+	$("#menuBag").click(function() {
+		if(bagFoldedOut == false){
+			$openFoldOut = TweenMax.to("#foldOut", 0.3, {right: 0});
+			$menuBag = TweenMax.to("#menuBag", 0.1, {backgroundColor: '#eeeeee'});
+			bagFoldedOut = true;
+		} else {
+			$closeFoldOut = TweenMax.to("#foldOut", 0.3, {right: -400});
+			$menuBag = TweenMax.to("#menuBag", 0.1, {backgroundColor: '#ffffff'});
+			bagFoldedOut = false;
+		}
+	});
+	$( window ).resize(function() {
+		resizeLogo();
+	});
+});
+
+function resizeLogo() {
+	$logoHeight = $('#master-text').height();
+	$('#slave-text').css('height', $logoHeight);
+}
