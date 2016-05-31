fed2426344f9bcc6e86a5072f3ae8b08084b42d5
Leo Ryberg <leo@sentientdesign.se>
2016-05-31 10:56:34 +0200

    Fixed conflicts, pushed new product detail page

--- javascript/app.js
+++ javascript/app.js
@@ -1,33 +1,6 @@
 $( document ).ready(function() {
-	jQuery('img.svg').each(function(){
-		var $img = jQuery(this);
-		var imgID = $img.attr('id');
-		var imgClass = $img.attr('class');
-		var imgURL = $img.attr('src');
-
-		jQuery.get(imgURL, function(data) {
-			// Get the SVG tag, ignore the rest
-			var $svg = jQuery(data).find('svg');
-
-			// Add replaced image's ID to the new SVG
-			if(typeof imgID !== 'undefined') {
-				$svg = $svg.attr('id', imgID);
-			}
-			// Add replaced image's classes to the new SVG
-			if(typeof imgClass !== 'undefined') {
-				$svg = $svg.attr('class', imgClass+' replaced-svg');
-			}
-
-			// Remove any invalid XML tags as per http://validator.w3.org
-			$svg = $svg.removeAttr('xmlns:a');
-
-			// Replace image with new SVG
-			$img.replaceWith($svg);
-
-		}, 'xml');
-	});
-
 	resizeLogo();
+	adaptHeights();
 	bagFoldedOut = false;
 
 	$("#menuBag").click(function() {
@@ -43,6 +16,7 @@ $( document ).ready(function() {
 	});
 	$( window ).resize(function() {
 		resizeLogo();
+		adaptHeights();
 	});
 	
 	
@@ -58,4 +32,15 @@ $( document ).ready(function() {
 function resizeLogo() {
 	$logoHeight = $('#master-text').height();
 	$('#slave-text').css('height', $logoHeight);
+}
+
+function adaptHeights() {
+	var $heightMasters = document.querySelectorAll("*[id^='height-master-']");
+	$.each($heightMasters, function(index, value) {
+		var $num = value.id.slice('height-master-'.length);
+		var $masterElem = $('#height-master-' + $num);
+		if($('.height-slave-' + $num)) {
+			$('.height-slave-' + $num).css('height', $masterElem.css('height'));
+		}
+	});
 }
